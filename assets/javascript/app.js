244f734c2adfe491babdef4ceab1d4e97bc28cc5
niles87 <nbing19@gmail.com>
2019-12-27 21:39:44 -0800

    added displayGifs function for adding gifs to html using ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,7 @@ function createButtons() {
   $("#added-tvshows").empty();
   for (var i = 0; i < tvShows.length; i++) {
     var buttons = `
-        <button class="tvshow" data-name="${tvShows[i]}">
+        <button class="tvshow-btn" data-name="${tvShows[i]}">
         ${tvShows[i]}
         </button>
         `;
@@ -26,5 +26,30 @@ function createButtons() {
     $("#added-tvshows").append(buttons);
   }
 }
+function displayGIFs() {
+  var show = $(this).attr("data-name");
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?api_key=XL0cvri4mdJxFaANOrpJHxQ6v0hiBPkp&q=" +
+    show +
+    "&limit=10&offset=0&rating=&lang=en";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function(response) {
+    for (var a = 0; a < response.data.length; a++) {
+      var gifDiv = `
+                  <div class="tvshow">
+                  <img src="${response.data[a].images.original_still.url}">
+                  </div>
+                
+                
+                `;
+      $("#gif-view").prepend(gifDiv);
+    }
+  });
+}
+
+$(document).on("click", ".tvshow-btn", displayGIFs);
 
 createButtons();
