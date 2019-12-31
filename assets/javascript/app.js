a4123b734f2e7d630595e908326821bdc309a60a
niles87 <nbing19@gmail.com>
2019-12-30 21:35:11 -0800

    added comments to to code, added but commented out cookie functions until later use

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,8 @@
 var tvShows = ["the office", "30 rock", "american horror story", "game of thrones"];
+// var user = prompt("Enter a username please:", "");
+// var newUser = false;
 
+// Adds user inputed text into tvShows array
 $("#add-show").on("click", function(event) {
   event.preventDefault();
 
@@ -13,6 +16,7 @@ $("#add-show").on("click", function(event) {
   createButtons();
 });
 
+// creates buttons for elements inside tvShows array
 function createButtons() {
   $("#added-tvshows").empty();
   for (var i = 0; i < tvShows.length; i++) {
@@ -26,6 +30,7 @@ function createButtons() {
   }
 }
 
+// API call to giphy using pressed button then adds it to the HTML document
 function displayGIFs() {
   var show = $(this).attr("data-name");
   var queryURL =
@@ -37,7 +42,7 @@ function displayGIFs() {
     url: queryURL,
     method: "GET",
   }).then(function(response) {
-    for (var a = 0; a < 10; a++) {
+    for (var q = 0; q < 10; q++) {
       var randomGIF = Math.floor(Math.random() * 100);
       var gifDiv = `
                   <div class="tvshow">
@@ -55,6 +60,8 @@ function displayGIFs() {
     }
   });
 }
+
+// creates the way to control the gif either play or pause
 function playPauseGif() {
   var gif = $(this).get(0);
   if (gif.paused) {
@@ -64,16 +71,62 @@ function playPauseGif() {
   }
 }
 
+// shows the favorites area then clones selected gif to the favorites area
 function addToFavorites() {
   $("#favorites").show();
   $("#favorites").append($(this).clone());
+  // setCookie(user, "favorites", 30);
 }
 
+// // sets a cookie
+// function setCookie(cn, cv, exd) {
+//   var day = new Date();
+//   day.setTime(day.getTime() + exd * 24 * 60 * 60 * 1000);
+//   var exp = "expires=" + day.toGMTString();
+
+//   document.cookie = cn + "=" + cv + ";" + exp + ";path=/;secure";
+// }
+
+// // gets cookie
+// function getCookie(cookieName) {
+//   var cname = cookieName + "=";
+//   var decodeCookie = decodeURIComponent(document.cookie);
+//   var cookieArray = decodeCookie.split(";");
+//   for (var j = 0; j < cookieArray.length; j++) {
+//     var cooki = cookieArray[j];
+//     while (cooki.charAt(0) == " ") {
+//       cooki = cooki.substring(1, cooki.length);
+//     }
+//     if (cooki.indexOf(cname) == 0) {
+//       var cookieSub = cookieArray.substring(cname.length, cooki.length);
+//       return cookieSub;
+//     }
+//   }
+//   return "";
+// }
+
+// // checks for a cookie
+// function checkCookie() {
+//   var user = getCookie("username");
+//   if (user != "") {
+//     alert("Welcome Back " + user);
+//   } else {
+//     newUser = true;
+//     if (user != "" && user != null) {
+//       setCookie("username", user, 30);
+//     }
+//   }
+// }
+
+// document functions
 $(document).on("click", "#tvshow-btn", displayGIFs);
 $(document).on("click", ".gif", playPauseGif);
 $(document).on("contextmenu", ".gif", addToFavorites);
-
 $(document).ready(function() {
+  // checkCookie();
+  // if (!newUser) {
   $("#favorites").hide();
+  // }
 });
+
 createButtons();
